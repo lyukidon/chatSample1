4bef893d93716abc53666c772909bccb3c18c673
lyukidon <qgx99098@gmail.com>
2022-01-22 11:00:19 +0000

    add files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const path=require('path');
+
+const app=express();
+
+app.set('port',process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+	res.render('index')
+})
+
+app.listen(app.get('port'),()=>{
+	console.log('port '+ app.get('port')+ ' 대기 중');
+})
