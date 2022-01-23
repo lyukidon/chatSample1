1ff330e7860444c021a6bf54d928f88b49d999ce
lyukidon <qgx99098@gmail.com>
2022-01-23 16:59:01 +0000

    웹에서 데이터 보내기 해야함

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const path=require('path');
 const connect=require('./schemas/index');
-const chatRouter=require('./routes');
+const indexRouter=require('./routes');
+const chatRouter=require('./routes/chat')
 connect();
 
 const app=express();
@@ -13,7 +14,9 @@ app.set('view engine', 'ejs');
 app.get('/', (req,res)=>{
 	res.render('index');
 })
-app.use('/api',chatRouter);
+app.use('/',indexRouter);
+app.use('/chat',chatRouter);
+app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(app.get('port'),()=>{
 	console.log('port '+ app.get('port')+ ' 대기 중');
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const router=express.Router();
 router.get('/',async (req,res,next)=>{
 	try{
 		const chats= await Chat.find({});
-		res.json({chats:chats});
+		console.log({chats:chats})
+		res.render('index',{chats:chats})
 	}catch(err){
 		console.error(err);
 		next(err);
 	};
 });
+
+router.post('/')
 
 module.exports=router;
